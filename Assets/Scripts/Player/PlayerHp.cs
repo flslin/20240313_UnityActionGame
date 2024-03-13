bdafdcc0541f76dbc9aacc733b5ea777cd02f974
flslin <84002788+flslin@users.noreply.github.com>
2024-03-13 17:04:50 +0900

    플레이어 상태 제작

--- Assets/Scripts/Player/PlayerHp.cs
+++ Assets/Scripts/Player/PlayerHp.cs
@@ -11,15 +11,46 @@ class PlayerHp : MonoBehaviour
     public AudioClip deathClip; // 플레이어 데미지 받았을 때 오디오
 
     Animator anim;
-    AudioClip playerAudio;
+    AudioSource playerAudio;
     PlayerMovement playerMovement;
 
     // 플레이어 죽었는지 판단
     bool isDead;
 
-    private void Start()
+    // 오브젝트 시작시 호출, Start보다 빨리 호출됨
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<PlayerMovement>();
+        currentHealth = startingHealth;
+        headlthSlider.value = currentHealth;
+    }
+
+    /// <summary>
+    /// 플레이어가 공격을 받았을 때 호출되는 함수
+    /// </summary>
+    /// <param name="amount">공격 받는 수치</param>
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        headlthSlider.value = currentHealth;
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+        else
+        {
+            anim.SetTrigger("Damage");
+        }
+    }
+
+    public void Death()
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
+        playerMovement.enabled = false;
     }
 
     private void Update()
