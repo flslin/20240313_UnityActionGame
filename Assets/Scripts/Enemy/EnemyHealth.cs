44fb55252767428d54bb7f0c7822fa9240826a27
flslin <84002788+flslin@users.noreply.github.com>
2024-03-14 09:27:36 +0900

    슬라임 만들기

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("체력 정보")]
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    [Header("색 변경")]
+    public float flashSpeed = 5.0f;
+    public Color flashColor = new Color(1, 0, 0, 0.1f);
+
+    [Header("사후 처리")]
+    public float sinkSpeed = 1.0f;
+
+    AudioSource enemyAudio;
+    // 슬라임의 상태에 따라 상황에 맞는 효과 전달용
+    bool isDead;
+    bool isSinking;
+    bool damaged;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
